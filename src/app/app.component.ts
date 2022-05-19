fbaec3fa017ec8d7e89dde1127dd46f9b5ea5e55
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-19 17:49:51 -0500

    Class 136: Cards and buttons with icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public name: string = 'Daniel';
+  public name: string = 'dAniel';
   changeName() {
     this.name = 'Otro nombre';
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
 import { AppComponent } from './app.component';
+
+// PrimeNG
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonModule],
+  imports: [BrowserModule, ButtonModule, CardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
