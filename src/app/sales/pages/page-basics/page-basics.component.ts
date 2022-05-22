2e39858e3991831a69b0d634cfeddbcb08aee8a0
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-21 20:12:32 -0500

    Class 144: DatePipe

--- src/app/sales/pages/page-basics/page-basics.component.ts
+++ src/app/sales/pages/page-basics/page-basics.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-basics',
   templateUrl: './page-basics.component.html',
-  styles: [],
+  styles: [
+    `
+      pre {
+        line-height: 130%;
+      }
+    `,
+  ],
 })
 export class PageBasicsComponent {
   public nameInLower: string = 'daniel';
   public nameInUpper: string = 'DANIEL';
   public completeName: string = 'daNiel GutiErreZ';
+  public date: Date = new Date();
 }
