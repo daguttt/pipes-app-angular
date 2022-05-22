bca9678ef1d97eb0d3272302bb0d1557341b1640
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-21 20:44:48 -0500

    Class 145: Change default language on pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { SalesModule } from './sales/sales.module';
 import { SharedModule } from './shared/shared.module';
 
+// Set language
+import { registerLocaleData } from '@angular/common';
+import localEsCO from '@angular/common/locales/es-CO';
+registerLocaleData(localEsCO);
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, SalesModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CO' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
