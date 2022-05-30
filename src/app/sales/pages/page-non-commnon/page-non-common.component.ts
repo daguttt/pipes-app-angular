23153c91e7269a2718bfb71d606a61265a1a129a
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-30 12:05:53 -0500

    Class 150: i18nSelectPipe

--- src/app/sales/pages/page-non-commnon/page-non-common.component.ts
+++ src/app/sales/pages/page-non-commnon/page-non-common.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-non-common',
   templateUrl: './page-non-common.component.html',
   styles: [],
 })
-export class PageNonCommonComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class PageNonCommonComponent {
+  public name: string = 'Jessica';
+  public gender: string = 'femenino';
+  public genderMap = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+  };
+  changeName(): void {
+    this.name === 'Jessica' ? (this.name = 'Daniel') : (this.name = 'Jessica');
+    this.gender === 'femenino'
+      ? (this.gender = 'masculino')
+      : (this.gender = 'femenino');
+  }
 }
