b86de7cb9a0b11bae2da6257ab78a796df15ef10
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-31 14:58:51 -0500

    Class 163: PrimeTable and PrimeToolbar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,8 +3,16 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
 import { FieldsetModule } from 'primeng/fieldset';
-
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 @NgModule({
-  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule],
+  exports: [
+    ButtonModule,
+    CardModule,
+    MenubarModule,
+    FieldsetModule,
+    TableModule,
+    ToolbarModule,
+  ],
 })
 export class PrimeNgModule {}
